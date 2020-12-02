8bba9edabdb7db51b87942750703ef878800483a
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-12-02 16:42:42 +0530

    Refactored : Code to display greeting message cards using ajax

--- greeting_message/greeting_message/settings.py
+++ greeting_message/greeting_message/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pykam18hw5#oxu4gvp#!ri-vbo1+9yquy0o^j0@ht_r*neoe_+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'FALSE'
